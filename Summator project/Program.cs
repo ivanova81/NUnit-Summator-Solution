be82589e30e6ef8dd6e19cc305c491ae711fa927
Monica Ivanova <98054187+ivanova81@users.noreply.github.com>
2022-05-10 12:57:32 +0300

    Add files via upload

---
+++ Summator project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Summator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sum = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+            if (sum == 21)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+            double average = Summator.Average(new int[] { 8, 13, 22 });
+            if (average == 14)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
